f989ae4759f391d204337999c8c39e024853ecc0
Deceptrax123 <srinitishsri@gmail.com>
2024-06-19 23:50:43 +0530

    design the classifier

--- Model/model.py
+++ Model/model.py
@@ -1,1 +1,17 @@
-#Network
+from torch.nn import Module
+from torch_geometric.nn import global_mean_pool
+from Model.ismorphism import GraphIsomorphismNetwork
+import torch.nn.functional as F
+
+
+class MoleculePropertyClassifier(Module):
+    def __init__(self):
+        super(MoleculePropertyClassifier, self).__init__()
+
+        self.nn = GraphIsomorphismNetwork()
+
+    def forward(self, v, edges, batch):
+        v = self.nn(v, edge_index=edges)
+        v = global_mean_pool(v, batch=batch)
+
+        return v, F.sigmoid(v)
